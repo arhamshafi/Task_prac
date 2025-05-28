82849462940907cf533e16afab8bc207cd3519d6
arham <arhambutt2923@gmail.com>
2025-05-28 12:31:08 +0500

    main_logic added

--- src/App.jsx
+++ src/App.jsx
@@ -4,40 +4,53 @@ import { MdEditSquare } from "react-icons/md";
 import { appcontext } from './Context';
 
 function App() {
-  let { inp , setinp } = useContext(appcontext)
+  let { inp, setinp, add_main, main } = useContext(appcontext)
   return (
     <div>
       <h1 className='w-full cursor-context-menu text-center text-black font-bold text-2xl mt-5 tracking-[2px]'>Own Task</h1>
 
       <div className='w-[90%] h-[40px] mt-5 mx-auto flex justify-center items-center gap-4'>
-        <input className='w-[80%] h-full px-4 text-lg bg-gray-100 text-black rounded-lg' type="text" 
+        <input className='w-[80%] h-full px-4 text-lg bg-gray-100 text-black rounded-lg' type="text"
           value={inp}
+          onChange={(e) => setinp(e.target.value)}
         />
-        <button className='py-1 px-3 bg-black text-white rounded-lg cursor-pointer h-full font-bold'>Add_Manager</button>
+        <button className='py-1 px-3 bg-black text-white rounded-lg cursor-pointer h-full font-bold'
+          onClick={() => add_main()}>Add_Manager</button>
       </div>
 
-      <div className='w-[90%] h-[450px] mx-auto mt-20 flex justify-between'>
-      
-        <div className='w-[380px] h-max py-3 rounded-lg px-3 border'>
-          <div className='w-full h-[70px] bg-black rounded-lg flex justify-between px-6 items-center'>
-            <h1 className='text-white font-bold text-lg tracking-[2px] cursor-context-menu'>Task etc</h1>
-            <div className='flex justify-center items-center  text-white text-2xl gap-2'>
-              <MdDelete className='text-red-500 cursor-pointer' />
-              <MdEditSquare className='text-blue-500 cursor-pointer' />
+      <div className='w-full h-[450px] mx-auto mt-8 flex-wrap flex justify-evenly'>
+
+        {main.map((ele, idx) => {
+          return (
+            <div key={idx} className='w-[380px] h-max py-3 mt-8 rounded-lg px-3 border'>
+              <div className='w-full h-[70px] bg-black rounded-lg flex justify-between px-6 items-center'>
+                <h1 className='text-white font-bold text-lg tracking-[2px] cursor-context-menu'>{ele.name}</h1>
+                <div className='flex justify-center items-center  text-white text-2xl gap-2'>
+                  <MdDelete className='text-red-500 cursor-pointer' />
+                  <MdEditSquare className='text-blue-500 cursor-pointer' />
+                </div>
+              </div>
+
+              <div className='w-full h-[240px] scrolldiv'>
+                {
+                  ele.todo.map((item, i) => {
+
+                    return (
+                      <div key={i} className='w-full h-[70px] bg-gray-200 rounded-lg mt-[10px]'></div>
+
+                    )
+                  })
+                }
+              </div>
+
+              <div className='w-full py-3 bg-black mt-3 rounded-lg'>
+                <input type="text" className='w-[80%] h-[35px] bg-white text-black px-3 rounded-lg mx-auto block' />
+                <button className='text-white font-white px-3 block mx-auto hover:bg-white hover:text-black mt-3 cursor-pointer h-[30px] rounded-lg border'>Add_Todo</button>
+              </div>
+
             </div>
-          </div>
-
-          <div className='w-full max-h-[240px] scrolldiv'>
-            <div className='w-full h-[70px] bg-gray-200 rounded-lg mt-[10px]'></div>
-           
-          </div>
-
-          <div className='w-full py-3 bg-black mt-3 rounded-lg'>
-            <input type="text" className='w-[80%] h-[35px] bg-white text-black px-3 rounded-lg mx-auto block' />
-            <button className='text-white font-white px-3 block mx-auto hover:bg-white hover:text-black mt-3 cursor-pointer h-[30px] rounded-lg border'>Add_Todo</button>
-          </div>
-
-        </div>
+          )
+        })}
       </div>
 
     </div>
--- src/Context.jsx
+++ src/Context.jsx
@@ -1,18 +1,43 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { createContext } from 'react'
 
 export const appcontext = createContext(  )
 
 function Context({children}) {
 
+
+    let data = localStorage.getItem("main_task") ? JSON.parse(localStorage.getItem("main_task")) :
+    [
+        { name : "TODO_LIST" , todo : [] },
+        { name : "IN PROGRESS" , todo : [] },
+        { name : "DONE" , todo : [] }
+    ] 
+    
+    
+    let [main , setmain] = useState(data)
     let [inp , setinp] = useState("")
     
+    console.log(main);
+    
 
+    function add_main(){
+      let update = [...main , { name : inp , todo : []}]
+      setmain(update)
+      setinp("")
+    }
+
+    useEffect(()=>{
+
+      localStorage.setItem("main_task",JSON.stringify(main))
+
+    },[main])
 
 
   return (
     <appcontext.Provider value={{
         inp,setinp,
+        add_main,
+        main
     }}>
         {children}
     </appcontext.Provider>
