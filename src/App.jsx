b55f461849df95dfa9831e3debb31ff9a8df6986
arham <arhambutt2923@gmail.com>
2025-08-24 12:02:37 +0500

    added new files

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { MdEditSquare } from "react-icons/md";
 import { appcontext } from './Context';
 
 function App() {
-  let { inp, setinp, add_main, main } = useContext(appcontext)
+  let { inp, setinp, add_main, main, edit_main, editng, del_main, add_todo, todo_inp, settodo_inp } = useContext(appcontext)
   return (
     <div>
       <h1 className='w-full cursor-context-menu text-center text-black font-bold text-2xl mt-5 tracking-[2px]'>Own Task</h1>
@@ -15,7 +15,7 @@ function App() {
           onChange={(e) => setinp(e.target.value)}
         />
         <button className='py-1 px-3 bg-black text-white rounded-lg cursor-pointer h-full font-bold'
-          onClick={() => add_main()}>Add_Manager</button>
+          onClick={() => add_main()}> {editng ? "Update" : "Add_Manager"} </button>
       </div>
 
       <div className='w-full h-[450px] mx-auto mt-8 flex-wrap flex justify-evenly'>
@@ -25,9 +25,9 @@ function App() {
             <div key={idx} className='w-[380px] h-max py-3 mt-8 rounded-lg px-3 border'>
               <div className='w-full h-[70px] bg-black rounded-lg flex justify-between px-6 items-center'>
                 <h1 className='text-white font-bold text-lg tracking-[2px] cursor-context-menu'>{ele.name}</h1>
-                <div className='flex justify-center items-center  text-white text-2xl gap-2'>
-                  <MdDelete className='text-red-500 cursor-pointer' />
-                  <MdEditSquare className='text-blue-500 cursor-pointer' />
+                <div className='flex justify-center items-center text-white text-2xl gap-2'>
+                  <MdEditSquare className='text-blue-500 cursor-pointer' onClick={() => edit_main(idx, ele)} />
+                  <MdDelete className='text-red-500 cursor-pointer' onClick={() => del_main(idx)} />
                 </div>
               </div>
 
@@ -36,7 +36,7 @@ function App() {
                   ele.todo.map((item, i) => {
 
                     return (
-                      <div key={i} className='w-full h-[70px] bg-gray-200 rounded-lg mt-[10px]'></div>
+                      <div key={i} className='w-full h-[70px] bg-gray-200 rounded-lg mt-[10px]'>{item}</div>
 
                     )
                   })
@@ -44,8 +44,16 @@ function App() {
               </div>
 
               <div className='w-full py-3 bg-black mt-3 rounded-lg'>
-                <input type="text" className='w-[80%] h-[35px] bg-white text-black px-3 rounded-lg mx-auto block' />
-                <button className='text-white font-white px-3 block mx-auto hover:bg-white hover:text-black mt-3 cursor-pointer h-[30px] rounded-lg border'>Add_Todo</button>
+                <input type="text" className='w-[80%] h-[35px] bg-white text-black px-3 rounded-lg mx-auto block'
+                  value={todo_inp[idx] || ""}
+                  onChange={(e) => {
+                    let clone = [...todo_inp]
+                    clone[idx] = e.target.value
+                    settodo_inp(clone)
+                  }}
+                />
+                <button className='text-white font-white px-3 block mx-auto hover:bg-white hover:text-black mt-3 cursor-pointer h-[30px] rounded-lg border'
+                  onClick={() => add_todo(idx)}>Add_Todo</button>
               </div>
 
             </div>
--- src/Context.jsx
+++ src/Context.jsx
@@ -1,45 +1,84 @@
 import React, { useEffect, useState } from 'react'
 import { createContext } from 'react'
 
-export const appcontext = createContext(  )
+export const appcontext = createContext()
 
-function Context({children}) {
+function Context({ children }) {
 
 
-    let data = localStorage.getItem("main_task") ? JSON.parse(localStorage.getItem("main_task")) :
+  let data = localStorage.getItem("main_task") ? JSON.parse(localStorage.getItem("main_task")) :
     [
-        { name : "TODO_LIST" , todo : [] },
-        { name : "IN PROGRESS" , todo : [] },
-        { name : "DONE" , todo : [] }
-    ] 
-    
-    
-    let [main , setmain] = useState(data)
-    let [inp , setinp] = useState("")
-    
-    console.log(main);
-    
+      { name: "TODO_LIST", todo: [] },
+      { name: "IN PROGRESS", todo: [] },
+      { name: "DONE", todo: [] }
+    ]
 
-    function add_main(){
-      let update = [...main , { name : inp , todo : []}]
+
+  let [main, setmain] = useState(data)
+  let [inp, setinp] = useState("")
+  let [editng, setediting] = useState(false)
+  let [edit_index, setedit_index] = useState(null)
+  let [todo_inp, settodo_inp] = useState([])
+  console.log(todo_inp);
+
+  function add_main() {
+    if (inp.trim() === "") return;
+    let update = [...main]
+
+    if (editng) {
+      update[edit_index].name = inp
+      setediting(false)
+    }
+    else {
+      update = [...main, { name: inp, todo: [] }]
       setmain(update)
-      setinp("")
     }
 
-    useEffect(()=>{
+    setinp("")
+  }
 
-      localStorage.setItem("main_task",JSON.stringify(main))
+  function edit_main(idx, ele) {
+    setedit_index(idx)
+    setediting(true)
+    setinp(ele.name)
+    scroll(0, 0)
+  }
 
-    },[main])
+  function del_main(idx) {
+    let update = [...main]
+    let fitr = update.filter((_, i) => i !== idx)
+    setmain(fitr)
+  }
 
+  function add_todo(idx) {
+    if (todo_inp[idx].trim() === "") return;
+
+    let update = [...main]
+
+    update[idx].todo.push(todo_inp[idx])
+    setmain(update)
+    let clone = [...todo_inp]
+    clone[idx] = ""
+    settodo_inp(clone)
+  }
+
+  useEffect(() => {
+    localStorage.setItem("main_task", JSON.stringify(main))
+    settodo_inp(new Array(main.length).fill(""))
+  }, [main])
 
   return (
     <appcontext.Provider value={{
-        inp,setinp,
-        add_main,
-        main
+      inp, setinp,
+      add_main,
+      main,
+      edit_main,
+      editng,
+      del_main,
+      add_todo,
+      todo_inp, settodo_inp,
     }}>
-        {children}
+      {children}
     </appcontext.Provider>
   )
 }
